d53f935c5188142a8ece9217ec7e0de95129715a
Olivia Hull <oliviahull0112@gmail.com>
2019-09-13 10:35:31 -0500

    FT fixed

--- Gaussian_Ehrenfest_Distance.py
+++ Gaussian_Ehrenfest_Distance.py
@@ -12,9 +12,11 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import sys
-import inspect
-import importlib
-import os
+from scipy.fftpack import fft, fftfreq
+from scipy.signal import find_peaks
+#import inspect
+#import importlib
+#import os
 
 '''
 Things to do:
@@ -22,27 +24,36 @@ Things to do:
 '''
 
 
-def my_main(filename, eF_type):
-    pos1=9
-    pos2=10
-    a1='N'
-    a2='N'
+#def my_main(filename, eF_type, pos1, pos2, a1, a2, doFT):
+def my_main(filename, pos1, pos2, a1, a2, doFT):
     fname = filename[:-4] # get rid of .log
-    coord_lines = get_nuc_coords(filename)
-    td = getDistance(coord_lines, pos1, pos2)
+    coord_lines, E_cons, eInfo_list = get_nuc_coords(filename)
+    eInfo = parseEInfoList(eInfo_list)
+    eInfo = str(eInfo)
+    print("eInfo: ", eInfo)
+    td, avgBL = getDistance(coord_lines, pos1, pos2)
+    calc_params, figOut = getCalcParams(fname, eInfo)
+    plotDistance(td, a1, a2, figOut, calc_params, avgBL)
+    if doFT == True:
+        FT_data = getDistFT(td)
+        plotDistFT(FT_data, a1, a2, figOut, calc_params)
+        plotECons(E_cons, figOut, calc_params) # only need to plot ECons once
+
+def getCalcParams(fname, eInfo):
+#pulls calculation parameters for plotting purposes
     params = fname.split("_")
    # eF_type = params[2][0:4] # e500, etc.
     orient = params[0] # 'par,' etc.
     polar = params[1] # e-field polarization, long or trans
 
     eFields= {
-      "e500" : "0.05",
-      "e450" : "0.045",
-      "e400" : "0.04",
-      "e300" : "0.03",
-      "e200" : "0.02",
-      "e100" : "0.01",
-      "e10"  : "0.001"
+       "0.05" : "e500", 
+       "0.045": "e450",
+       "0.04" : "e400",
+       "0.03" : "e300",
+       "0.02" : "e200",
+       "0.01" : "e100",
+       "0.001": "e10" 
       }
 
     orientations = {
@@ -55,16 +66,16 @@ def my_main(filename, eF_type):
       "trans" : "transverse"
       }
     
-    
-    efield_label = eFields[eF_type]
+    eF_type = eFields[eInfo]
+    efield_label = eInfo
     orientation_label = orientations[orient]
     polarization_label = polarizations[polar]
     calc_params = efield_label + " a.u. " + polarization_label+"-polarized electric field, " \
                   + orientation_label + " orientation"
 
     figOut=fname + "_" + eF_type + ".png"
-    plotDistance(td, a1, a2, figOut, calc_params)
 
+    return calc_params, figOut
 
 def get_nuc_coords(filename):
     ''' Pull the nuclear coordinates from an ehrenfest dynamics calculation in Gaussian
@@ -74,6 +85,8 @@ def get_nuc_coords(filename):
     
     count = 1
     coord_lines = [count]
+    E_cons = []
+    eInfo_list = []
     
     with open(filename, 'r') as f:
         for line in f:
@@ -88,12 +101,25 @@ def get_nuc_coords(filename):
                     else:
                         coord_lines.append([coord.split()[1], coord.split()[3], coord.split()[4], coord.split()[5]])
                         coord = next(f)
+            if (' Energy = ' in line ):
+                E_cons.append(float(line.split()[-1]))
+            if ('                            Ex =' in line):
+                e_line = line
+                for i in range(3):
+                    eInfo_list.append(e_line.split()[2])
+                    e_line = next(f)
+
     f.close()
     
-    return coord_lines                    
+    return coord_lines, E_cons, eInfo_list                    
     
     '''Now all of the coordinate data is in coord_lines'''
 
+def parseEInfoList(eInfo_list):
+    for eF in eInfo_list:
+        if float(eF) != 0:
+            eInfo = float(eF)
+    return eInfo
     
 def getDistance(coord_lines, pos1, pos2):
     #pos1 = 9 # 9th position in coord list
@@ -104,7 +130,6 @@ def getDistance(coord_lines, pos1, pos2):
     while type(coord_lines[nA+1]) == list:
         nA = nA+1
     
-    # preallocate
     x1 = [] ; x2 = [] ; xd = []
     y1 = [] ; y2 = [] ; yd = []
     z1 = [] ; z2 = [] ; zd = []
@@ -137,19 +162,22 @@ def getDistance(coord_lines, pos1, pos2):
         
         j =  j + i
         ti = ti + 1 # time step index  
-        t_fs = t_fs + 0.0005999 # put time in units of fs  
+        t_fs = t_fs + .005 # put time in units of fs  
+        # NOTE: this is currently a hacky bandaid to get fs
+        # if your nuclear time step (stepsize in input file) is not
+        # stepsize=1000, then you must manually change this value
 
     td = np.zeros((len(t), 2))
     td[:, 0] = np.array(t)
     td[:, 1] = np.array(d)
+    avgBL = np.mean(d[4010:]) # cut out the steps that have the field on
 
-    return td
+    return td, avgBL
 
-def plotDistance(td, a1, a2, figOut, calc_params):
+def plotDistance(td, a1, a2, figOut, calc_params, avgBL):
     '''
+    https://stackoverflow.com/questions/1713335/peak-finding-algorithm-for-python-scipy
     '''
-    a1 = 'N'
-    a2 = 'N'
     
     plt.figure()
     
@@ -157,16 +185,82 @@ def plotDistance(td, a1, a2, figOut, calc_params):
     plt.xlabel("Time (fs)")
     plt.ylabel("Distance (Angstrom)")
 
-    plt.suptitle(a1+"-"+a2+" bond distance over time", y=1.05, fontsize=18)
+    peaks, _ = find_peaks(td[:,1], prominence=.01)
+    difs = []
+    for i in range(len(peaks)-1):
+        difs.append(td[:,0][peaks[i+1]]-td[:,0][peaks[i]])
+    avgBL =  "%.3f" % avgBL
+    plt.suptitle(a1+"-"+a2+" distance over time (avg: "+ avgBL +" Angstrom)" , y=1.05, fontsize=18)
     plt.title(calc_params, fontsize=10)    
 
-    plt.savefig(figOut, format='png', dpi=300, bbox_inches='tight')
-    
+    plt.savefig(figOut[:-4]+"_"+a1+a2+".png", format='png', dpi=300, bbox_inches='tight')
+
+def getDistFT(td):
+    '''
+    td is already in units of fs
+    '''
+    td = td[4010:] # cut out the electric field part
+    fw = fft(td[:,1])
+    n = len(td[:,1])
+    t = td[:,0]
+    timestep = t[0] - t[1]
+    w = fftfreq(n, d=timestep) #*2.0*np.pi
+    w = w*(1e15)/(2.9979e10)
+    S = abs(fw)
+# convert this to cm-1:
+# also, put in the average bond length
+# also make plots for Ag-N
+# get energy conservation as well
+   # w = (w*27.2114) # give freqs in eV
+
+    FT_data = np.transpose([w,S])
+    #FT_data = [w, S]
+
+    return FT_data
+
+def plotDistFT(FT_data, a1, a2, figOut, calc_params):
+    plt.figure()
+    pk=[]
+    peaks, _ = find_peaks(FT_data[:,1], prominence=10)
+    yvals=FT_data[:,1]
+    xvals=FT_data[:,0]
+    for peak in peaks:
+        if peak > 500:
+            pk.append(peak)
+    mpk=len(xvals) - 1
+    for peak in pk:
+        if yvals[peak] >= yvals[mpk] and xvals[peak] > 0:
+            mpk = peak
+    ax = plt.subplot(111)
+    plt.plot(xvals, yvals)
+    plt.xlim([0,5000])
+    plt.ylim([0, yvals[mpk]+10])
+    plt.xlabel("Wavenumber (cm-1)")
+    plt.ylabel("Intensity (a.u.)")
+    plt.suptitle("Fourier transform of "+ a1+"-"+a2+" bond distance over time", y=1.05, fontsize=18)
+    label_peak = "%.3f" % xvals[mpk]
+    plt.title(calc_params +", peak: "+ label_peak + " cm-1", fontsize=10)
+    plt.savefig(figOut[:-4]+"_"+a1+a2+"_FT.png", format='png', dpi=300, bbox_inches='tight')
+
+def plotECons(E_cons, figOut, calc_params):
+    plt.figure()
+    ax = plt.gca()
+#    ax.get_xaxis().get_major_formatter().set_useOffset(False)
+    plt.plot(E_cons)
+    plt.gca().yaxis.set_major_formatter(matplotlib.ticker.StrMethodFormatter("{x:.3f}"))
+    plt.suptitle("Energy conservation", y=1.05, fontsize=18)
+    plt.ylabel("Energy (a.u.)")
+    plt.xlabel("Timestep")
+    plt.title(calc_params)
+    plt.savefig(figOut[:-4]+"_ECons.png", format='png', dpi=300, bbox_inches='tight')
+
+
 
 filename = sys.argv[1]
-eF_type = sys.argv[2]
-
-my_main(filename, eF_type)
+pos1=9;pos2=10;a1='N';a2='N';doFT=True
+my_main(filename, pos1, pos2, a1, a2, doFT)
+pos1=8;pos2=9;a1='Ag';a2='N'; doFT=False
+my_main(filename, pos1, pos2, a1, a2, doFT)
 #!/usr/bin/env python
 #make executable in bash chmod +x PyRun
 # https://stackoverflow.com/questions/3987041/run-function-from-the-command-line
